74779b78ec0b9434bfeaa66086736e78cbf1b4f3
jomppe1 <joona.pigg@gmail.com>
2017-04-26 15:24:32 +0300

    aklsafah

--- app/src/main/java/com/example/jomppe/googlesigninoikee/SignInActivity.java
+++ app/src/main/java/com/example/jomppe/googlesigninoikee/SignInActivity.java
@@ -1,15 +1,20 @@
 package com.example.jomppe.googlesigninoikee;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.LinearLayout;
+import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.gms.auth.api.Auth;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
@@ -20,13 +25,28 @@ import com.google.android.gms.common.SignInButton;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.common.api.ResultCallback;
 import com.google.android.gms.common.api.Status;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.GetTokenResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 
-public class SignInActivity extends AppCompatActivity implements View.OnClickListener,GoogleApiClient.OnConnectionFailedListener{
+public class SignInActivity extends AppCompatActivity implements GoogleApiClient.OnConnectionFailedListener{
 
 
     private GoogleApiClient mGoogleApiClient;
     private static final int RC_SIGN_IN = 9001;
+    private Button buttonRegister;
+    private SignInButton googleSignIn;
+    private EditText editTextEmail;
+    private EditText editTextPassword;
+    private TextView textViewSignin;
+
+
+    private ProgressDialog progressDialog;
+
+    private FirebaseAuth firebaseAuth;
 
 
 
@@ -36,7 +56,41 @@ public class SignInActivity extends AppCompatActivity implements View.OnClickLis
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
 
-        findViewById(R.id.sign_in_button).setOnClickListener(this);
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        progressDialog = new ProgressDialog(this);
+
+        googleSignIn = (SignInButton)findViewById(R.id.sign_in_button);
+        googleSignIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                        signIn();
+            }
+        });
+
+
+        editTextEmail = (EditText)findViewById(R.id.editTextEmail);
+        editTextPassword = (EditText)findViewById(R.id.editTextPassword);
+
+        buttonRegister = (Button) findViewById(R.id.buttonRegister);
+        buttonRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (v == buttonRegister){
+                    registerUser();
+                }
+            }
+        });
+
+        textViewSignin = (TextView)findViewById(R.id.textViewSingin);
+        textViewSignin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (v == textViewSignin){
+                    //will open login activity here
+                }
+            }
+        });
 
 
         // Configure sign-in to request the user's ID, email address, and basic
@@ -54,15 +108,43 @@ public class SignInActivity extends AppCompatActivity implements View.OnClickLis
 
     }
 
+    public void registerUser(){
+        String email = editTextEmail.getText().toString().trim();
+        String password = editTextPassword.getText().toString().trim();
 
-    @Override
-    public void onClick(View v) {
-        switch (v.getId()) {
-            case R.id.sign_in_button:
-                signIn();
-                break;
-            // ...
+        if (TextUtils.isEmpty(email)){
+            //Email is empty
+            Toast.makeText(this,"Syötä sähköpostiosoite",Toast.LENGTH_SHORT).show();
+            //Stopping the function execution further
+            return;
         }
+        if (TextUtils.isEmpty(password)){
+            //Password is empty
+            Toast.makeText(this,"Syötä salasana",Toast.LENGTH_SHORT).show();
+            //Stopping the function execution further
+            return;
+        }
+        //if validations is ok
+        //We will first show a progressDialog
+
+        progressDialog.setMessage("Rekisteröidään käyttäjää...");
+        progressDialog.show();
+
+        firebaseAuth.createUserWithEmailAndPassword(email,password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()){
+                            //user is succesfully registered and logged in
+                            //we start the profile activity
+                            //right now lets display a toast only
+                            Toast.makeText(SignInActivity.this, "Rekisteröinti onnistui", Toast.LENGTH_SHORT).show();
+                        }else{
+                            Toast.makeText(SignInActivity.this, "Rekisteröinti epäonnistui... Yritä uudelleen!", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+
     }
     @Override
     public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
