ae5594097828c5f58d842da514a7ca870969d59a
jomppe1 <joona.pigg@gmail.com>
2017-04-26 19:36:49 +0300

    IsoMuutos

--- app/src/main/java/com/example/jomppe/googlesigninoikee/FireBsSignUp.java
+++ app/src/main/java/com/example/jomppe/googlesigninoikee/FireBsSignUp.java
@@ -3,16 +3,12 @@ package com.example.jomppe.googlesigninoikee;
 import android.app.ProgressDialog;
 import android.content.Intent;
 import android.support.annotation.NonNull;
-import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.TextUtils;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.LinearLayout;
-import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -23,16 +19,13 @@ import com.google.android.gms.auth.api.signin.GoogleSignInResult;
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.SignInButton;
 import com.google.android.gms.common.api.GoogleApiClient;
-import com.google.android.gms.common.api.ResultCallback;
-import com.google.android.gms.common.api.Status;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
-import com.google.firebase.auth.GetTokenResult;
 import com.google.firebase.auth.FirebaseAuth;
 
 
-public class SignInActivity extends AppCompatActivity implements GoogleApiClient.OnConnectionFailedListener{
+public class FireBsSignUp extends AppCompatActivity{
 
 
     private GoogleApiClient mGoogleApiClient;
@@ -54,20 +47,18 @@ public class SignInActivity extends AppCompatActivity implements GoogleApiClient
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_sign_in);
+        setContentView(R.layout.activity_firebssignup);
+
+
+//        if (firebaseAuth.getCurrentUser() != null){
+//            //profile activity here
+//            finish();
+//            startActivity(new Intent(getApplicationContext(),ProfileActivity.class));
+//        }
 
         firebaseAuth = FirebaseAuth.getInstance();
 
         progressDialog = new ProgressDialog(this);
-
-        googleSignIn = (SignInButton)findViewById(R.id.sign_in_button);
-        googleSignIn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                        signIn();
-            }
-        });
-
 
         editTextEmail = (EditText)findViewById(R.id.editTextEmail);
         editTextPassword = (EditText)findViewById(R.id.editTextPassword);
@@ -86,25 +77,12 @@ public class SignInActivity extends AppCompatActivity implements GoogleApiClient
         textViewSignin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (v == textViewSignin){
                     //will open login activity here
-                }
+                Intent intentin = new Intent(FireBsSignUp.this, FireBsSignIn.class);
+                FireBsSignUp.this.startActivity(intentin);
+
             }
         });
-
-
-        // Configure sign-in to request the user's ID, email address, and basic
-        // profile. ID and basic profile are included in DEFAULT_SIGN_IN.
-        GoogleSignInOptions signInOptions = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-                .requestEmail()
-                .build();
-
-        // Build a GoogleApiClient with access to the Google Sign-In API and the
-        // options specified by gso.
-        mGoogleApiClient = new GoogleApiClient.Builder(this)
-                .enableAutoManage(this,this)
-                .addApi(Auth.GOOGLE_SIGN_IN_API, signInOptions)
-                .build();
 
     }
 
@@ -138,67 +116,15 @@ public class SignInActivity extends AppCompatActivity implements GoogleApiClient
                             //user is succesfully registered and logged in
                             //we start the profile activity
                             //right now lets display a toast only
-                            Toast.makeText(SignInActivity.this, "Rekisteröinti onnistui", Toast.LENGTH_SHORT).show();
+                            finish();
+                            startActivity(new Intent(getApplicationContext(),ProfileActivity.class));
                         }else{
-                            Toast.makeText(SignInActivity.this, "Rekisteröinti epäonnistui... Yritä uudelleen!", Toast.LENGTH_SHORT).show();
+                            Toast.makeText(FireBsSignUp.this, "Rekisteröinti epäonnistui... Yritä uudelleen!", Toast.LENGTH_SHORT).show();
                         }
                     }
                 });
 
     }
-    @Override
-    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
-
-    }
-
-
-    private void signIn() {
-        Intent signInIntent = Auth.GoogleSignInApi.getSignInIntent(mGoogleApiClient);
-        startActivityForResult(signInIntent, RC_SIGN_IN);
-    }
-
-    @Override
-    public void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-
-        Intent intent3 = new Intent(SignInActivity.this, MyFood.class);
-        SignInActivity.this.startActivity(intent3);
-
-        // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
-        if (requestCode == RC_SIGN_IN) {
-            GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
-            handleSignInResult(result);
-            Intent intent2 = new Intent(SignInActivity.this, MyFood.class);
-            SignInActivity.this.startActivity(intent2);
-        }
-    }
-
-    private void handleSignInResult(GoogleSignInResult result) {
-        if (result.isSuccess()) {
-            // Signed in successfully, show authenticated UI.
-            GoogleSignInAccount acct = result.getSignInAccount();
-            updateUI(true);
-
-        } else {
-            // Signed out, show unauthenticated UI.
-            updateUI(false);
-        }
-    }
-
-    private void updateUI(boolean isLogin)
-    {
-        if (isLogin)
-        {
-        }
-        else
-        {
-        }
-
-    }
-
-
-
-
 
 }
 
---
+++ app/src/main/java/com/example/jomppe/googlesigninoikee/GoogleFBLogIn.java
@@ -0,0 +1,137 @@
+package com.example.jomppe.googlesigninoikee;
+
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.auth.api.signin.GoogleSignInResult;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.GoogleApiClient;
+
+
+public class GoogleFBLogIn extends AppCompatActivity implements GoogleApiClient.OnConnectionFailedListener{
+
+
+    private GoogleApiClient mGoogleApiClient;
+    private static final int RC_SIGN_IN = 9001;
+    private Button buttonEkaActivity;
+    private SignInButton googleSignIn;
+    private TextView textViewSignin;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_googlefblogin);
+
+
+        googleSignIn = (SignInButton)findViewById(R.id.sign_in_button);
+        googleSignIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                signIn();
+            }
+        });
+
+
+
+        buttonEkaActivity = (Button) findViewById(R.id.buttonRegister);
+        buttonEkaActivity.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent Logini = new Intent(GoogleFBLogIn.this, FireBsSignIn.class);
+                GoogleFBLogIn.this.startActivity(Logini);
+            }
+        });
+
+        textViewSignin = (TextView)findViewById(R.id.textViewSinginEka);
+        textViewSignin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                    //will open login activity here
+                Intent Logini = new Intent(GoogleFBLogIn.this, FireBsSignIn.class);
+                GoogleFBLogIn.this.startActivity(Logini);
+
+            }
+        });
+
+
+        // Configure sign-in to request the user's ID, email address, and basic
+        // profile. ID and basic profile are included in DEFAULT_SIGN_IN.
+        GoogleSignInOptions signInOptions = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build();
+
+        // Build a GoogleApiClient with access to the Google Sign-In API and the
+        // options specified by gso.
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .enableAutoManage(this,this)
+                .addApi(Auth.GOOGLE_SIGN_IN_API, signInOptions)
+                .build();
+
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
+    }
+
+
+    private void signIn() {
+        Intent signInIntent = Auth.GoogleSignInApi.getSignInIntent(mGoogleApiClient);
+        startActivityForResult(signInIntent, RC_SIGN_IN);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        Intent intent3 = new Intent(GoogleFBLogIn.this, MyFood.class);
+        GoogleFBLogIn.this.startActivity(intent3);
+
+        // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+            GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
+            handleSignInResult(result);
+            Intent intent2 = new Intent(GoogleFBLogIn.this, MyFood.class);
+            GoogleFBLogIn.this.startActivity(intent2);
+        }
+    }
+
+    private void handleSignInResult(GoogleSignInResult result) {
+        if (result.isSuccess()) {
+            // Signed in successfully, show authenticated UI.
+            GoogleSignInAccount acct = result.getSignInAccount();
+            updateUI(true);
+
+        } else {
+            // Signed out, show unauthenticated UI.
+            updateUI(false);
+        }
+    }
+
+    private void updateUI(boolean isLogin)
+    {
+        if (isLogin)
+        {
+        }
+        else
+        {
+        }
+
+    }
+
+
+
+
+
+}
+
