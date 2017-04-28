a24f96dcf5d6af54d00770517150c299587457af
jomppe1 <joona.pigg@gmail.com>
2017-04-28 16:58:08 +0300

    Tietojen tallennus

--- app/src/main/java/com/example/jomppe/googlesigninoikee/ProfileActivity.java
+++ app/src/main/java/com/example/jomppe/googlesigninoikee/ProfileActivity.java
@@ -5,10 +5,14 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class ProfileActivity extends AppCompatActivity {
 
@@ -16,6 +20,13 @@ public class ProfileActivity extends AppCompatActivity {
 
     private TextView textViewUserEmail;
     private Button buttonLogout;
+
+    private DatabaseReference databaseReference;
+
+    private EditText editTextName, editTextAddress, editTextNumber;
+
+    private Button buttonSave;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,16 +39,22 @@ public class ProfileActivity extends AppCompatActivity {
 //            startActivity(new Intent(this, FireBsSignIn.class));
 //        }
 
+        databaseReference = FirebaseDatabase.getInstance().getReference();
 
+        editTextAddress = (EditText)findViewById(R.id.editTextAddress);
+        editTextName = (EditText)findViewById(R.id.editTextName);
+        editTextNumber = (EditText)findViewById(R.id.editTextNumber);
+        buttonSave = (Button) findViewById(R.id.buttonSave);
 
         FirebaseUser user = firebaseAuth.getCurrentUser();
 
+        //initializing Views
+        buttonLogout = (Button) findViewById(R.id.buttonLogout);
         textViewUserEmail = (TextView)findViewById(R.id.textViewUserEmail);
-
+        //Displaying logged in user name
         textViewUserEmail.setText("Tervetuloa " +user.getEmail());
 
-
-        buttonLogout = (Button) findViewById(R.id.buttonLogout);
+        //Adding listener to buttons
 
         buttonLogout.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -49,5 +66,34 @@ public class ProfileActivity extends AppCompatActivity {
             }
         });
 
+        buttonSave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                saveUserInformation();
+            }
+        });
+
     }
+
+     private void saveUserInformation(){
+
+         String name = editTextName.getText().toString().trim();
+         String address = editTextAddress.getText().toString().trim();
+         String numero = editTextNumber.getText().toString().trim();
+
+         UserInformation userInformation = new UserInformation(name, address, numero);
+
+         FirebaseUser user = firebaseAuth.getCurrentUser();
+
+         databaseReference.child(user.getUid()).setValue(userInformation);
+
+         Toast.makeText(this,"Tiedot tallennettiin onnistuneesti!", Toast.LENGTH_LONG).show();
+
+     }
+
+
+
+
+
+
 }
---
+++ app/src/main/java/com/example/jomppe/googlesigninoikee/UserInformation.java
@@ -0,0 +1,23 @@
+package com.example.jomppe.googlesigninoikee;
+
+/**
+ * Created by Jomppe on 28.4.2017.
+ */
+
+public class UserInformation {
+
+    public String name;
+    public String address;
+    public String numero;
+
+    public UserInformation(){
+
+    }
+
+    public UserInformation(String name, String address,String numero) {
+        this.name = name;
+        this.address = address;
+        this.numero = numero;
+    }
+}
+
